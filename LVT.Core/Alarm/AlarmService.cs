d8f4d07e50dcb7d8ffad0e893ac39d492e0fe738
Syed Zaryab Haider <zaryabsyed009@gmail.com>
2020-02-11 10:34:05 +0500

    Add files via upload

---
+++ LVT.Core/Alarm/AlarmService.cs
@@ -0,0 +1,49 @@
+﻿using LVT.Data.RepositoryWrapper;
+using LVT.Models.Entities;
+using LVT.Models.Filters;
+using System;
+using System.Collections.Generic;
+
+
+namespace LVT.Core.Alarm
+{
+    public class AlarmService : IAlarmService
+    {
+        private IRepositoryWrapper _repositoryWrapper;
+
+        public AlarmService(IRepositoryWrapper repositoryWrapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+        }
+
+        public List<VehicleAlarm> GetAll(VehicleAlarmsFilter vehicleAlarmFilter)
+        {
+            try
+            {
+                return _repositoryWrapper.VehicleAlarms.GetPaged(vehicleAlarmFilter.PageNo, vehicleAlarmFilter.PageSize);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public int PushAlarm(VehicleAlarm vehicleAlarm)
+        {
+            int rowsEffected = 0;
+            try
+            {
+                _repositoryWrapper.VehicleAlarms.Create(vehicleAlarm);
+                rowsEffected = _repositoryWrapper.Save();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+            return rowsEffected;
+        }
+    }
+}
---
+++ LVT.Core/Alarm/IAlarmService.cs
@@ -0,0 +1,16 @@
+﻿
+using LVT.Models.Entities;
+using LVT.Models.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LVT.Core.Alarm
+{
+    public interface IAlarmService
+    {
+        int PushAlarm(VehicleAlarm vehicleAlarm);
+
+        List<VehicleAlarm> GetAll(VehicleAlarmsFilter vehicleAlarmFilter);
+    }
+}
