de555dabb52e45aad452553c352c53aa583f0b99
Syed Zaryab Haider <zaryabsyed009@gmail.com>
2020-02-11 11:05:15 +0500

    Add files via upload

---
+++ LVT.Web/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using LVT.Web.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LVT.Web.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureRepositoryWrapper(this IServiceCollection services)
+        {
+
+            services.AddScoped<IAPIClientUtility, APIClientUtility>();
+
+        }
+    }
+}
